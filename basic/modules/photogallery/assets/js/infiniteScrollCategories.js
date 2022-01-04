8da9907d98452ec0540262340948cdc5fae72c40
PaulMerkushevich <11mpa@mail.ru>
2022-01-04 16:47:37 +0300

    1.3.6SG

--- basic/modules/photogallery/assets/js/infiniteScrollCategories.js
+++ basic/modules/photogallery/assets/js/infiniteScrollCategories.js
@@ -1,7 +1,8 @@
 var loadCategory = false;
 
-window.addEventListener('load', function () {
-    $(window).scroll(function () {
+
+$(window).scroll(function () {
+    window.addEventListener('load', function () {
         if ($(window).height() + $(window).scrollTop() >= $(".category-grid").height() - 200 && !loadCategory) {
             loadCategory = true;
             infiniteScroll();
@@ -50,8 +51,8 @@ function infiniteScroll() {
 function viewNextCategories(data) {
     data.forEach(function (category) {
         if (typeof category['imagePath'] !== 'undefined') {
-            let imagePath = siteUrl+category['imagePath'];
-            categoryElement = '<a class="gallery-item" href="'+siteUrl+'/web/page/category/' + category['slug'] + '", style="text-decoration: none;"><div class="category-info-container">  <div class="small-image" style="background: url(\'' + imagePath + '\'); background-size: cover;">   <div style="display: flex; align-items: center; justify-content: center; background-color: rgba(0,0,0,.60); width:100%; height: 25%;">   <span style="color: whitesmoke;"><span class="category-title">' + category['title'] + '</span> <span class="badge bg-secondary category-count" style="opacity: 0.8; border-radius: 6px;">' + category['count'] + '</span></span></div></div></div></a>';
+            let imagePath = siteUrl + category['imagePath'];
+            categoryElement = '<a class="gallery-item" href="' + siteUrl + '/web/page/category/' + category['slug'] + '", style="text-decoration: none;"><div class="category-info-container">  <div class="small-image" style="background: url(\'' + imagePath + '\'); background-size: cover;">   <div style="display: flex; align-items: center; justify-content: center; background-color: rgba(0,0,0,.60); width:100%; height: 25%;">   <span style="color: whitesmoke;"><span class="category-title">' + category['title'] + '</span> <span class="badge bg-secondary category-count" style="opacity: 0.8; border-radius: 6px;">' + category['count'] + '</span></span></div></div></div></a>';
 
             $(".category-grid").append(categoryElement);
             setTimeout(function () {
